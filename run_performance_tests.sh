7d995dde35d7cbce1d37c632a6a8e22b6af63988
David Wybourn <dwybourn@users.noreply.github.com>
2018-12-18 11:56:51 +0000

    HTBHF-22 Checking that necessary variables are set. Removing setting … (#12)
    * HTBHF-22 Checking that necessary variables are set. Removing setting of results directory variable.

--- run_performance_tests.sh
+++ run_performance_tests.sh
@@ -3,6 +3,25 @@
 export PATH=$PATH:${BIN_DIR}
 export CURRENT_DIR=`pwd`
 
+check_variable_is_set(){
+    if [[ -z ${!1} ]]; then
+        echo "$1 must be set and non empty"
+        exit 1
+    fi
+}
+
+check_variable_is_set BIN_DIR
+check_variable_is_set GATLING_FOLDER_NAME
+check_variable_is_set GATLING_URL
+check_variable_is_set CF_API
+check_variable_is_set CF_USER
+check_variable_is_set CF_PASS
+check_variable_is_set CF_ORG
+check_variable_is_set CF_SPACE
+check_variable_is_set CF_PUBLIC_DOMAIN
+check_variable_is_set RESULTS_DIRECTORY
+check_variable_is_set PERF_TESTS_DIR
+
 if [[ ! -e ${BIN_DIR}/${GATLING_FOLDER_NAME} ]]; then
   echo "Downloading gatling"
   mkdir -p ${BIN_DIR}
@@ -21,9 +40,6 @@ ROUTE_PREFIX=$(cat /dev/urandom | tr -dc 'a-z' | fold -w 16 | head -n 1)
 cf map-route ${UI_APP_NAME} ${CF_PUBLIC_DOMAIN} --hostname ${ROUTE_PREFIX}
 export BASE_URL="https://$ROUTE_PREFIX.${CF_PUBLIC_DOMAIN}/help-to-buy-healthy-foods"
 
-
-RESULTS_DIRECTORY=`pwd`/${PERF_TESTS_RESULTS_DIRECTORY}
-
-${BIN_DIR}/${GATLING_FOLDER_NAME}/bin/gatling.sh -sf ${PERF_TESTS_DIRECTORY}/uk/gov/dhsc/htbhf --run-description "Performance tests" --results-folder ${RESULTS_DIRECTORY}
+${BIN_DIR}/${GATLING_FOLDER_NAME}/bin/gatling.sh -sf ${PERF_TESTS_DIR}/uk/gov/dhsc/htbhf --run-description "Performance tests" --results-folder ${RESULTS_DIRECTORY}
 
 cf delete-route -f ${CF_PUBLIC_DOMAIN} --hostname=${ROUTE_PREFIX}
